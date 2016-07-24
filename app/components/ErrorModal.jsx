a870415d58e5e06dfb8ecf475759c95a0ad289d8
Aaron Arney <aaron.g.arney@gmail.com>
2016-07-24 10:20:09 -0400

    Add defaultProps to ErrorModal

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -1,6 +1,17 @@
 var React = require('react');
 
 var ErrorModal = React.createClass({
+  getDefaultProps: function () {
+    return {
+      title: 'Error'
+    }
+  },
+
+  propTypes: {
+    title: React.PropTypes.string,
+    message: React.PropTypes.string.isRequired
+  },
+
   componentDidMount: function () {
     var modal = new Foundation.Reveal($('#error-modal'));
     modal.open();
